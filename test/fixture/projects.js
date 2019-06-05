35f5cb2b1dbacf8f2936f1b0de9538736a2e9b3f
Maximilian Lenkeit <maximilian.lenkeit@gmail.com>
2019-06-05 08:46:39 +0200

    chore: replace eslint with standardjs

--- test/fixture/projects.js
+++ test/fixture/projects.js
@@ -1,8 +1,8 @@
-'use strict';
+'use strict'
 
 module.exports = [{
   'name': 'mlenkeit/pi-dab-test',
   'dir': 'pi-dab-test',
   'githubWebhook': 16107911,
   'postCheckoutScript': 'npm install'
-}];
+}]
--- test/unit/update-github-webhook.spec.js
+++ test/unit/update-github-webhook.spec.js
@@ -1,38 +1,38 @@
-'use strict';
+/* eslint-env mocha */
+'use strict'
 
-const chai = require('chai');
-const expect = require('chai').expect;
-const nock = require('nock');
+const chai = require('chai')
+const expect = require('chai').expect
+const nock = require('nock')
 
-chai.use(require('chai-as-promised'));
+chai.use(require('chai-as-promised'))
 
-describe('update-github-webhook', function() {
-  
-  beforeEach(function() {
+describe('update-github-webhook', function () {
+  beforeEach(function () {
     this.project = {
       name: 'some-user/some-repo',
       githubWebhook: 1234
-    };
-    this.projects = [this.project];
-    
-    this.url = 'abc';
-    this.githubUser = 'some-user';
-    this.githubToken = 'some-token';
-    this.secret = 's3cret';
-    
+    }
+    this.projects = [this.project]
+
+    this.url = 'abc'
+    this.githubUser = 'some-user'
+    this.githubToken = 'some-token'
+    this.secret = 's3cret'
+
     this.update = require('./../../lib/update-github-webhook')({
       githubToken: this.githubToken,
       githubUser: this.githubUser,
       projects: this.projects,
       secret: this.secret
-    });
-  });
-  
-  afterEach(function() {
-    nock.cleanAll();
-  });
-  
-  it('updates the webhook with the given url', function() {
+    })
+  })
+
+  afterEach(function () {
+    nock.cleanAll()
+  })
+
+  it('updates the webhook with the given url', function () {
     const scope = nock('https://api.github.com')
       .patch(`/repos/${this.project.name}/hooks/${this.project.githubWebhook}`, {
         config: {
@@ -44,29 +44,28 @@ describe('update-github-webhook', function() {
       })
       .basicAuth({
         user: this.githubUser,
-        pass: this.githubToken,
+        pass: this.githubToken
       })
-      .reply(200);
-      
+      .reply(200)
+
     return this.update(this.url).then(() => {
-      expect(scope.isDone()).to.equal(true);
-    });
-  });
-  
-  it('rejects the promise then update fails with bad status code', function() {
+      expect(scope.isDone()).to.equal(true)
+    })
+  })
+
+  it('rejects the promise then update fails with bad status code', function () {
     nock('https://api.github.com')
       .patch(`/repos/${this.project.name}/hooks/${this.project.githubWebhook}`)
-      .reply(500);
-      
-    return expect(this.update(this.url)).to.be.rejected;
-  });
-  
-  it('rejects the promise then update fails with an error', function() {
+      .reply(500)
+
+    return expect(this.update(this.url)).to.be.rejected
+  })
+
+  it('rejects the promise then update fails with an error', function () {
     nock('https://api.github.com')
       .patch(`/repos/${this.project.name}/hooks/${this.project.githubWebhook}`)
-      .replyWithError('socket hang up');
-      
-    return expect(this.update(this.url)).to.be.rejected;
-  });
-  
-});
+      .replyWithError('socket hang up')
+
+    return expect(this.update(this.url)).to.be.rejected
+  })
+})
