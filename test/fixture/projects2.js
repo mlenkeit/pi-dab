f058b91c9a75e97c9b68a9a913aec7d702e7da00
Maximilian Lenkeit <maximilian.lenkeit@sap.com>
2019-06-06 08:36:50 +0200

    test: run system tests in tmp dir

---
+++ test/fixture/projects2.js
@@ -0,0 +1,9 @@
+'use strict'
+
+module.exports = [{
+  'name': 'mlenkeit/pi-dab-test',
+  'cloneUrl': 'https://github.com/mlenkeit/pi-dab-test.git',
+  // 'dir': 'pi-dab-test',
+  'githubWebhook': 16107911,
+  'postCheckoutScript': 'npm install'
+}]
