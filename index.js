efd0577b2b03a38e56843549e9d1cada848b361b
Maximilian Lenkeit <maximilian.lenkeit@gmail.com>
2019-06-06 08:36:50 +0200

    test: run system tests in tmp dir

--- index.js
+++ index.js
@@ -1,25 +1,33 @@
 'use strict'
 
+const check = require('check-types')
 const path = require('path')
 const winston = require('winston')
 
+winston.log('info', 'starting pi-dab')
 const GITHUB_TOKEN = process.env.GITHUB_TOKEN
 const GITHUB_USER = process.env.GITHUB_USER
-const PORT = parseInt(process.env.PORT, 10)
-const PROJECTS = process.env.PROJECTS
+check.assert.nonEmptyString(GITHUB_USER, 'environment variable GITHUB_USER may not be empty')
+check.assert.nonEmptyString(GITHUB_TOKEN, 'environment variable GITHUB_TOKEN may not be empty')
+winston.log('info', 'using GitHub user %s with token ******', GITHUB_USER)
 
-const projectJsonDirname = path.dirname(PROJECTS)
+const PORT = parseInt(process.env.PORT, 10)
+check.assert.positive(PORT, `environment variable PORT must be a positive number; found ${process.env.PORT}`)
+
+const PROJECTS = process.env.PROJECTS
+check.assert.nonEmptyString(PROJECTS, 'environment variable PROJECTS may not be empty')
+const PROJECTS_ROOT_DIR = process.env.PROJECTS_ROOT_DIR || '.projects'
+winston.log('info', 'loading projects from %s', PROJECTS)
+winston.log('info', 'storing projects in %s', PROJECTS_ROOT_DIR)
 const projects = require(PROJECTS)
-  .map(project => {
-    project.dir = path.resolve(projectJsonDirname, project.dir)
-    return project
-  })
+winston.log('info', `projects: %j`, projects)
 
 const piDab = require('./lib/index')({
   GITHUB_TOKEN: GITHUB_TOKEN,
   GITHUB_USER: GITHUB_USER,
   PORT: PORT,
-  projects: projects
+  projects: projects,
+  PROJECTS_ROOT_DIR: path.resolve(process.cwd(), PROJECTS_ROOT_DIR)
 })
 
 piDab.start()
