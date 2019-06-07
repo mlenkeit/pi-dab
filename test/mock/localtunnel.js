1536568edb61683422f012edf99aa631c02d9905
Maximilian Lenkeit <maximilian.lenkeit@gmail.com>
2019-06-07 19:33:35 +0200

    fix(open-tunnel): close tunnel before opening again

--- test/mock/localtunnel.js
+++ test/mock/localtunnel.js
@@ -4,18 +4,16 @@ const EventEmitter = require('events')
 const sinon = require('sinon')
 
 module.exports = sinon.spy(function () {
-  const tunnel = {
-    url: 'http://localhost.localtunnel'
-  }
-  const tunnelEvents = new EventEmitter()
+  const tunnel = new EventEmitter()
+  tunnel.url = 'http://localhost.localtunnel'
+  tunnel.close = sinon.spy()
 
   const mock = sinon.spy((port, cb) => {
     cb(mock.err, tunnel)
-    return tunnelEvents
+    return tunnel
   })
 
   mock.tunnel = tunnel
-  mock.tunnelEvents = tunnelEvents
   mock.err = null
 
   return mock
