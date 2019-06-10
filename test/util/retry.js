4bdc137fa8bd3fafe15644738781955689faef85
Maximilian Lenkeit <maximilian.lenkeit@gmail.com>
2019-06-10 11:01:34 +0200

    refactor(retry): log original error

--- test/util/retry.js
+++ test/util/retry.js
@@ -3,7 +3,7 @@
 const wait = require('./wait')
 
 const retry = module.exports = (tries, fn, delay, tryCounter = 0) => fn()
-  .catch((/* err */) =>
+  .catch(err =>
     tries > tryCounter
       ? wait(delay).then(() => retry(tries, fn, delay, tryCounter + 1))
-      : Promise.reject(new Error(`Operation failed after re-trying ${tries} times`)))
+      : Promise.reject(new Error(`Operation failed after re-trying ${tries} times: ${err}`)))
