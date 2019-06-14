0df49f8f9be1c82b1fe2352e1088b619e6474f44
Maximilian Lenkeit <maximilian.lenkeit@gmail.com>
2019-06-14 18:03:06 +0200

    feat(pi-dab): show portion of github token

--- index.js
+++ index.js
@@ -4,12 +4,14 @@ const check = require('check-types')
 const path = require('path')
 const winston = require('winston')
 
+const obfuscateString = require('./lib/obfuscate-string')
+
 winston.log('info', 'starting pi-dab')
 const GITHUB_TOKEN = process.env.GITHUB_TOKEN
 const GITHUB_USER = process.env.GITHUB_USER
 check.assert.nonEmptyString(GITHUB_USER, 'environment variable GITHUB_USER may not be empty')
 check.assert.nonEmptyString(GITHUB_TOKEN, 'environment variable GITHUB_TOKEN may not be empty')
-winston.log('info', 'using GitHub user %s with token ******', GITHUB_USER)
+winston.log('info', 'using GitHub user %s with token %s', GITHUB_USER, obfuscateString(GITHUB_TOKEN))
 
 const PORT = parseInt(process.env.PORT, 10)
 check.assert.positive(PORT, `environment variable PORT must be a positive number; found ${process.env.PORT}`)
