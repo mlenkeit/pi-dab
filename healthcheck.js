2c60206a47c51636eb3fc5f5038d4fe3d4eff072
Maximilian Lenkeit <maximilian.lenkeit@gmail.com>
2019-06-17 06:43:51 +0200

    feat(docker): add healthcheck

---
+++ healthcheck.js
@@ -0,0 +1,63 @@
+'use strict'
+
+const check = require('check-types')
+const winston = require('winston')
+
+const rp = require('request-promise-native')
+
+const obfuscateString = require('./lib/obfuscate-string')
+
+winston.log('info', 'running healthcheck')
+const GITHUB_TOKEN = process.env.GITHUB_TOKEN
+const GITHUB_USER = process.env.GITHUB_USER
+check.assert.nonEmptyString(GITHUB_USER, 'environment variable GITHUB_USER may not be empty')
+check.assert.nonEmptyString(GITHUB_TOKEN, 'environment variable GITHUB_TOKEN may not be empty')
+winston.log('debug', 'using GitHub user %s with token %s', GITHUB_USER, obfuscateString(GITHUB_TOKEN))
+
+const PROJECTS = process.env.PROJECTS
+check.assert.nonEmptyString(PROJECTS, 'environment variable PROJECTS may not be empty')
+const PROJECTS_ROOT_DIR = process.env.PROJECTS_ROOT_DIR || '.projects'
+winston.log('debug', 'loading projects from %s', PROJECTS)
+winston.log('debug', 'storing projects in %s', PROJECTS_ROOT_DIR)
+const projects = require(PROJECTS)
+
+const piDabProject = projects.find(project => project.name === 'mlenkeit/pi-dab')
+if (!piDabProject) {
+  winston.log('error', 'could not find configuration for mlenkeit/pi-dab project')
+  process.exit(1)
+}
+
+const getWebhookUrl = ({ repoName, webhookId, githubUser, githubToken }) => rp.get({
+  url: `https://api.github.com/repos/${repoName}/hooks/${webhookId}`,
+  json: true,
+  auth: {
+    username: githubUser,
+    password: githubToken
+  },
+  headers: {
+    'User-Agent': 'pi-dab-healthcheck'
+  },
+  transform: body => body.config.url
+})
+
+getWebhookUrl({
+  repoName: piDabProject.name,
+  webhookId: piDabProject.githubWebhook,
+  githubUser: GITHUB_USER,
+  githubToken: GITHUB_TOKEN
+}).then(webhookUrl => {
+  winston.log('debug', 'found webhook url %s', webhookUrl)
+  return rp.get({
+    uri: webhookUrl,
+    resolveWithFullResponse: true
+  })
+}).then(response => {
+  if (response.statusCode === 200) {
+    winston.log('info', 'healthcheck successful')
+    return process.exit(0)
+  }
+  throw new Error(`Unexpected status code ${response.statusCode} instead of 200`)
+}).catch(err => {
+  winston.log('error', 'healthcheck failed: %s', err)
+  return process.exit(1)
+})
