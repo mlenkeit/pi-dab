51055c63a67d77f207aefeebdc91f7158a42c454
Maximilian Lenkeit <maximilian.lenkeit@gmail.com>
2019-07-01 08:36:32 +0200

    feat(healthcheck): use shared logger config

--- healthcheck.js
+++ healthcheck.js
@@ -1,29 +1,28 @@
 'use strict'
 
 const check = require('check-types')
-const winston = require('winston')
-
 const rp = require('request-promise-native')
 
+const logger = require('./lib/logger')()
 const obfuscateString = require('./lib/obfuscate-string')
 
-winston.log('info', 'running healthcheck')
+logger.log('info', 'running healthcheck')
 const GITHUB_TOKEN = process.env.GITHUB_TOKEN
 const GITHUB_USER = process.env.GITHUB_USER
 check.assert.nonEmptyString(GITHUB_USER, 'environment variable GITHUB_USER may not be empty')
 check.assert.nonEmptyString(GITHUB_TOKEN, 'environment variable GITHUB_TOKEN may not be empty')
-winston.log('debug', 'using GitHub user %s with token %s', GITHUB_USER, obfuscateString(GITHUB_TOKEN))
+logger.log('debug', 'using GitHub user %s with token %s', GITHUB_USER, obfuscateString(GITHUB_TOKEN))
 
 const PROJECTS = process.env.PROJECTS
 check.assert.nonEmptyString(PROJECTS, 'environment variable PROJECTS may not be empty')
 const PROJECTS_ROOT_DIR = process.env.PROJECTS_ROOT_DIR || '.projects'
-winston.log('debug', 'loading projects from %s', PROJECTS)
-winston.log('debug', 'storing projects in %s', PROJECTS_ROOT_DIR)
+logger.log('debug', 'loading projects from %s', PROJECTS)
+logger.log('debug', 'storing projects in %s', PROJECTS_ROOT_DIR)
 const projects = require(PROJECTS)
 
 const piDabProject = projects.find(project => project.name === 'mlenkeit/pi-dab')
 if (!piDabProject) {
-  winston.log('error', 'could not find configuration for mlenkeit/pi-dab project')
+  logger.log('error', 'could not find configuration for mlenkeit/pi-dab project')
   process.exit(1)
 }
 
@@ -46,18 +45,18 @@ getWebhookUrl({
   githubUser: GITHUB_USER,
   githubToken: GITHUB_TOKEN
 }).then(webhookUrl => {
-  winston.log('debug', 'found webhook url %s', webhookUrl)
+  logger.log('debug', 'found webhook url %s', webhookUrl)
   return rp.get({
     uri: webhookUrl,
     resolveWithFullResponse: true
   })
 }).then(response => {
   if (response.statusCode === 200) {
-    winston.log('info', 'healthcheck successful')
+    logger.log('info', 'healthcheck successful')
     return process.exit(0)
   }
   throw new Error(`Unexpected status code ${response.statusCode} instead of 200`)
 }).catch(err => {
-  winston.log('error', 'healthcheck failed: %s', err)
+  logger.log('error', 'healthcheck failed: %s', err)
   return process.exit(1)
 })
